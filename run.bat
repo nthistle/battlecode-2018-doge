27a8ca79b91f148a0354878a7ec4737dac438c1f
Evan Shi <evanyshi@gmail.com>
2018-01-10 18:44:48 -0500

    deprecated

--- run.bat
+++ run.bat
@@ -1,8 +1,3 @@
 docker pull battlecode/battlecode-2018
 
-docker stop $(docker ps -q)
-docker container rm $(docker container ls -aq)
-docker volume rm $(docker volume ls -q)$
-docker volume prune -f
-
 docker run -it --privileged -p 16147:16147 -p 6147:6147 -v "%~dp0:/player" --rm "battlecode/battlecode-2018"
