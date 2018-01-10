501d4a4a73e1f419ce0ab6d60f9fb2e48946c8e8
Evan Shi <evanyshi@gmail.com>
2018-01-10 18:15:29 -0500

    testing clean (window still to be done)

--- run.bat
+++ run.bat
@@ -1,3 +1,8 @@
 docker pull battlecode/battlecode-2018
 
-docker run -it --privileged -p 16147:16147 -p 6147:6147 -v "%~dp0:/player" "battlecode/battlecode-2018"
+docker stop $(docker ps -q)
+docker container rm $(docker container ls -aq)
+docker volume rm $(docker volume ls -q)$
+docker volume prune
+
+docker run -it --privileged -p 16147:16147 -p 6147:6147 -v "%~dp0:/player" --rm "battlecode/battlecode-2018"
