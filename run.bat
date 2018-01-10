dc452e38165d8f7b56fa88f5d6d7cf862e5752fa
Evan Shi <evanyshi@gmail.com>
2018-01-10 18:20:36 -0500

    no prompt for confirmation

--- run.bat
+++ run.bat
@@ -3,6 +3,6 @@ docker pull battlecode/battlecode-2018
 docker stop $(docker ps -q)
 docker container rm $(docker container ls -aq)
 docker volume rm $(docker volume ls -q)$
-docker volume prune
+docker volume prune -f
 
 docker run -it --privileged -p 16147:16147 -p 6147:6147 -v "%~dp0:/player" --rm "battlecode/battlecode-2018"
