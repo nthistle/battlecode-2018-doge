5de7c8377622f220a6bb89e4893a95abc4715927
Neil Thistlethwaite <neilt1111@yahoo.com>
2018-01-11 01:51:21 -0500

    Update Utils

--- structure-base/Utils.java
+++ structure-base/Utils.java
@@ -6,4 +6,9 @@ public class Utils
     public static Direction getRandomDirection(Direction[] dirs, Random rng) {
         return dirs[rng.nextInt(dirs.length)];
     }
+    
+    public static Team getOtherTeam(Team thisTeam) {
+        if(thisTeam==Team.Blue) return Team.Red;
+        return Team.Blue;
+    }
 }
