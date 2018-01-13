9226385fbe35a68d0c5d812afd3df32db3432aad
Aneesh Kotnana <aneesh.kotnana@gmail.com>
2018-01-12 23:16:33 -0500

    Add work-in-progress ranger+sniper

---
+++ testplayer_ranger/Player.java
@@ -0,0 +1,82 @@
+import bc.*;
+import java.util.Random;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Player {
+    
+    public static void main(String[] args) {
+    
+        Random rand = new Random();
+        
+        GameController gc = new GameController();
+        Team myTeam = gc.team();
+        
+        Map<Integer,UnitHandler> myHandler = new HashMap<Integer,UnitHandler>();
+
+        // lets queue up research for rangers
+        for(int i = 0; i < 3; i++)
+            gc.queueResearch(UnitType.Ranger);
+
+        if(gc.planet() == Planet.Mars) {
+            while(true) { gc.nextTurn(); } // temporary to fix some of this logic from bugging on mars
+        }
+
+        while (true) {
+        
+            System.out.println("Round #"+gc.round());
+            
+            VecUnit units = gc.myUnits();
+            
+            for(int i = 0; i < units.size(); i ++) {
+                registerUnit(gc, rand, myHandler, units.get(i));
+            }
+            
+            // all factories should move first, so we unload then move
+            for(int i = 0; i < units.size(); i ++) {
+                try {
+                    Unit unit = units.get(i);
+                    if(unit.unitType()==UnitType.Factory)
+                        myHandler.get(unit.id()).takeTurn(unit);
+                } catch(Exception e) {}
+            }
+            
+            // now everything else can move
+            for(int i = 0; i < units.size(); i ++) {
+                try {
+                    Unit unit = units.get(i);
+                    if(unit.unitType()!=UnitType.Factory)
+                        myHandler.get(unit.id()).takeTurn(unit);
+                } catch(Exception e) {}
+            }
+            
+            gc.nextTurn();
+        }
+    }
+    
+    // registers the unit into the hashmap with a new handler, if it isn't
+    // already in there.
+    public static void registerUnit(GameController gc, Random rand, Map<Integer,UnitHandler> handlermap, Unit unit) {
+   
+        if(!handlermap.containsKey(unit.id())) {
+        
+            UnitHandler newHandler = null;
+            
+            switch(unit.unitType()) {
+                case Factory:
+                  newHandler = new FactoryHandler(gc, unit.id(), rand);
+                  break;
+                case Ranger:
+                  newHandler = new RangerHandler(gc, unit.id(), rand);
+                  break;
+                case Worker:
+                  newHandler = new WorkerHandler(gc, unit.id(), rand);
+                  break;
+                default:
+                  break;
+            }
+            
+            handlermap.put(unit.id(), newHandler);
+        }
+    }
+}
