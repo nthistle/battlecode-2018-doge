760b771ad6d81b74ab61c43e3301bc503fcfaba0
Aneesh Kotnana <aneesh.kotnana@gmail.com>
2018-01-14 16:18:58 -0500

    Update controller swarm logic

--- task-swarm-logic/FactoryHandler.java
+++ task-swarm-logic/FactoryHandler.java
@@ -3,11 +3,28 @@ import java.util.Random;
 
 public class FactoryHandler extends UnitHandler {
 
+	public static int rangersCreated = 0;
+
     public FactoryHandler(PlanetController parent, GameController gc, int id, Random rng) {
         super(parent, gc, id, rng);
     }
     
     public void takeTurn() {
-        // TODO
+        if(this.rangersCreated < 8) {
+        	if(gc.canProduceRobot(this.id, UnitType.Ranger)) {
+            	gc.produceRobot(this.id, UnitType.Ranger);
+        	}
+	        
+        	this.rangersCreated++;
+        }
+        VecUnitID garrison = unit.structureGarrison();
+        if(garrison.size() > 0) {
+            for(int i = 0; i < 8; i ++) {
+                Direction unloadDir = Utils.getRandomDirection(Direction.values(), this.rng);
+                if(gc.canUnload(this.id, unloadDir)) {
+                    gc.unload(this.id, unloadDir);
+                }
+            }
+        }
     }
 }
--- task-swarm-logic/WorkerHandler.java
+++ task-swarm-logic/WorkerHandler.java
@@ -3,11 +3,44 @@ import java.util.Random;
 
 public class WorkerHandler extends UnitHandler {
 
+	public static int factoriesCreated = 0;
+	private int curFactoryCooldown = 0;
+    private static final int FACTORY_COOLDOWN = 50;
+
     public WorkerHandler(PlanetController parent, GameController gc, int id, Random rng) {
         super(parent, gc, id, rng);
     }
     
     public void takeTurn() {
-        // TODO
+		VecUnit nearby = gc.senseNearbyUnits(unit.location().mapLocation(), 2); // immediate range
+        for(int i = 0; i < nearby.size(); i ++) {
+            if(gc.canBuild(this.id, nearby.get(i).id())) {
+                gc.build(this.id, nearby.get(i).id());
+                return;
+            }
+        }
+        
+        // cooldown starts ticking after *done* building 
+        if(curFactoryCooldown > 0) curFactoryCooldown --;
+        
+        // can't build anything, try moving
+        if(gc.isMoveReady(this.id)) {
+            for(int i = 0; i < 5; i ++) {
+                Direction moveDir = Utils.getRandomDirection(Direction.values(), this.rng);
+                if(gc.canMove(this.id, moveDir)) {
+                    gc.moveRobot(this.id, moveDir);
+                    break;
+                }
+            }
+        }
+        
+        for(int i = 0; i < 5; i ++) {
+            Direction bpDir = Utils.getRandomDirection(Direction.values(), this.rng);
+            if(this.factoriesCreated < 1 && gc.canBlueprint(this.id, UnitType.Factory, bpDir)) {
+                gc.blueprint(this.id, UnitType.Factory, bpDir);
+                this.curFactoryCooldown = FACTORY_COOLDOWN;
+                this.factoriesCreated++;
+            }
+        }
     }
 }
