299e58ae664445a24382992755abddba5235e71e
Neil Thistlethwaite <neilt1111@yahoo.com>
2018-01-14 14:22:34 -0500

    Start pathfinding wip

---
+++ task-pathfinding/EarthController.java
@@ -0,0 +1,59 @@
+import bc.*;
+import java.util.Random;
+import java.util.HashMap;
+import java.util.Random;
+
+public class EarthController extends PlanetController
+{
+    public EarthController(GameController gc, Random rng) {
+        super(gc, rng);
+    }
+    
+    public void control() {
+    
+        System.out.println("Earth Controller iniatied");
+    
+        HashMap<Integer,UnitHandler> myHandler = new HashMap<Integer,UnitHandler>();
+
+        while (true) {
+        
+            System.out.println("Round #"+gc.round());
+            
+            VecUnit units = gc.myUnits();
+            
+            for(int i = 0; i < units.size(); i ++) {
+                Unit unit = units.get(i);
+                
+                if(!myHandler.containsKey(unit.id())) {
+                
+                    UnitHandler newHandler = null;
+                    
+                    switch(unit.unitType()) {
+                        case Factory:
+                          newHandler = new FactoryHandler(this, gc, unit.id(), rng);
+                          break;
+                        case Knight:
+                          newHandler = new KnightHandler(this, gc, unit.id(), rng);
+                          break;
+                        case Ranger:
+                          newHandler = new RangerHandler(this, gc, unit.id(), rng);
+                          break;
+                        case Worker:
+                          newHandler = new WorkerHandler(this, gc, unit.id(), rng);
+                          break;
+                        default:
+                          break;
+                    }
+                    
+                    myHandler.put(unit.id(), newHandler);
+                }
+                myHandler.get(unit.id()).takeTurn(unit);
+            }
+            gc.nextTurn();
+        }
+    }
+    
+    public Planet getPlanet() {
+        return Planet.Earth;
+    }
+}
---
+++ task-pathfinding/KnightHandler.java
@@ -0,0 +1,13 @@
+import bc.*;
+import java.util.Random;
+
+public class KnightHandler extends UnitHandler {
+
+    public KnightHandler(PlanetController parent, GameController gc, int id, Random rng) {
+        super(parent, gc, id, rng);
+    }
+    
+    public void takeTurn() {
+        // TODO
+    }
+}
