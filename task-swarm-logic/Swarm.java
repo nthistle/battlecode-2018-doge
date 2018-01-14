6a0fdd8d5eef6c21f152ba809e282544f9f1a60b
Aneesh Kotnana <aneesh.kotnana@gmail.com>
2018-01-14 14:42:40 -0500

    Add swarm abstract class + in-progress RangerSwarm

---
+++ task-swarm-logic/Swarm.java
@@ -0,0 +1,58 @@
+import bc.*;
+import java.util.Random;
+import java.util.List;
+import java.util.ArrayList;
+
+public abstract class Swarm
+{
+    protected final GameController gc;
+    protected List<Integer> unitIDs;
+    protected final int MIN_SWARM_SIZE = 8;
+    protected MapLocation swarmTarget; //the final target of the path
+    protected Path currPath; //temporary
+    protected MapLocation swarmLeader; //robots should be following the swarmLeader
+    protected boolean swarmIsMoving = false;
+
+    public Swarm() {
+        this.unitIDs = new ArrayList<Integer>();
+    }
+    
+    public void addUnit(int unitID) {
+        this.unitIDs.add(unitID);
+    }
+
+    public void removeUnit(int unitID) {
+        this.unitIDs.remove(unitID);
+    }
+
+    public List<Integer> getUnits() {
+        return this.unitIDs;
+    }
+
+    public boolean isSwarm() {
+        return this.unitIDs.size() >= this.MIN_SWARM_SIZE;
+    }
+
+    public boolean isTogether() {
+        for(int i = 0; i < unitIDs.size(); i++) {
+            if(!Utils.canMoveWiggle(this.gc, unitIDs.get(i), unitIDs.get(i).location().mapLocation().directionTo(swarmLeader))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void setSwarmLeader(MapLocation target) {
+        this.swarmLeader = target;
+    }
+
+    public void setSwarmTarget(MapLocation target) {
+        this.swarmTarget = target;
+    }
+
+    public abstract void doTurn();
+    
+    public abstract void moveToLeader();
+
+    public abstract void moveToTarget(Path path);
+}
