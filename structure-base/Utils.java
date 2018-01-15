33af3594ca543ad0df1a33b14cf35f7e3429ec72
Neil Thistlethwaite <neilt1111@yahoo.com>
2018-01-14 20:44:43 -0500

    Update utils in structure-base

--- structure-base/Utils.java
+++ structure-base/Utils.java
@@ -24,6 +24,21 @@ public class Utils
             Direction.Northeast
         };
     }
+
+    public static Direction reverseDirection(Direction dir) {
+        switch(dir) {
+            case North: return Direction.South;
+            case Northwest: return Direction.Southeast;
+            case West: return Direction.East;
+            case Southwest: return Direction.Northeast;
+            case South: return Direction.North;
+            case Southeast: return Direction.Northwest;
+            case East: return Direction.West;
+            case Northeast: return Direction.Southwest;
+            default: break;
+        }
+        return Direction.Center;
+    }
     
     // lazy way
     public static Direction[] getAdjacentDirs(Direction dir) {
