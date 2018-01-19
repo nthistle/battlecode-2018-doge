4061972e1046203a0902e06f9eb9b66169b1d21e
Evan Shi <evanyshi@gmail.com>
2018-01-19 17:40:15 -0500

    start seeding bot

---
+++ seeding-bot/FactoryHandler.java
@@ -0,0 +1,13 @@
+import bc.*;
+import java.util.Random;
+
+public class FactoryHandler extends UnitHandler {
+
+    public FactoryHandler(PlanetController parent, GameController gc, int id, Random rng) {
+        super(parent, gc, id, rng);
+    }
+    
+    public void takeTurn() {
+        // TODO
+    }
+}
---
+++ seeding-bot/Utils.java
@@ -0,0 +1,100 @@
+import bc.*;
+import java.util.Random;
+
+public class Utils
+{
+    public static Direction getRandomDirection(Direction[] dirs, Random rng) {
+        return dirs[rng.nextInt(dirs.length)];
+    }
+    
+    public static Team getOtherTeam(Team thisTeam) {
+        if(thisTeam==Team.Blue) return Team.Red;
+        return Team.Blue;
+    }
+
+    public static Direction[] directions() {
+        return new Direction[] {
+            Direction.North,
+            Direction.Northwest,
+            Direction.West,
+            Direction.Southwest,
+            Direction.South,
+            Direction.Southeast,
+            Direction.East,
+            Direction.Northeast
+        };
+    }
+
+    public static Direction reverseDirection(Direction dir) {
+        switch(dir) {
+            case North: return Direction.South;
+            case Northwest: return Direction.Southeast;
+            case West: return Direction.East;
+            case Southwest: return Direction.Northeast;
+            case South: return Direction.North;
+            case Southeast: return Direction.Northwest;
+            case East: return Direction.West;
+            case Northeast: return Direction.Southwest;
+            default: break;
+        }
+        return Direction.Center;
+    }
+    
+    // lazy way
+    public static Direction[] getAdjacentDirs(Direction dir) {
+        switch(dir) {
+          case North:
+            return new Direction[] {Direction.Northwest, Direction.Northeast};
+          case Northeast:
+            return new Direction[] {Direction.North, Direction.East};
+          case East:
+            return new Direction[] {Direction.Northeast, Direction.Southeast};
+          case Southeast:
+            return new Direction[] {Direction.East, Direction.South};
+          case South:
+            return new Direction[] {Direction.Southeast, Direction.Southwest};
+          case Southwest:
+            return new Direction[] {Direction.South, Direction.West};
+          case West:
+            return new Direction[] {Direction.Southwest, Direction.Northwest};
+          case Northwest:
+            return new Direction[] {Direction.West, Direction.North};
+          default:
+            break;
+        }
+        return null;
+    }
+    
+    // tries to move in dir, if fail, trys two adjacent dirs
+    // returns 0 if move failed, 1 if original dir succeeded,
+    // 2 if counter-clockwise neighbor succeeded,
+    // 3 if clockwise neighbor succeeded
+    public static int tryMoveWiggle(GameController gc, int unitId, Direction dir) {
+        if(!gc.isMoveReady(unitId)) {
+            return 0;
+        }
+        if(gc.canMove(unitId, dir)) {
+            gc.moveRobot(unitId, dir);
+            return 1;
+        }
+        Direction[] neighboring = getAdjacentDirs(dir);
+        if(gc.canMove(unitId, neighboring[0])) {
+            gc.moveRobot(unitId, neighboring[0]);
+            return 2;
+        }
+        if(gc.canMove(unitId, neighboring[1])) {
+            gc.moveRobot(unitId, neighboring[1]);
+            return 3;
+        }
+        return 0;
+    }
+
+    public static boolean compareMapLocation(MapLocation a, MapLocation b) {
+        return a.getPlanet() == b.getPlanet() && a.getX() == b.getX() && a.getY() == b.getY();
+    }
+
+    public static boolean canMoveWiggle(GameController gc, int unitId, Direction dir) {
+        Direction[] neighboring = getAdjacentDirs(dir);
+        return gc.canMove(unitId, dir) || gc.canMove(unitId, neighboring[0]) || gc.canMove(unitId, neighboring[1]);
+    }
+}
