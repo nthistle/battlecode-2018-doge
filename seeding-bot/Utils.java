e0c6ad1c355493944d2a4183a3f14da0aa6af243
Evan Shi <evanyshi@gmail.com>
2018-01-19 19:11:36 -0500

    start transition and clean for testing

--- seeding-bot/Utils.java
+++ seeding-bot/Utils.java
@@ -1,4 +1,7 @@
 import bc.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Random;
 
 public class Utils
@@ -38,6 +41,10 @@ public class Utils
             default: break;
         }
         return Direction.Center;
+    }
+    
+    public static long distanceSquaredTo(MapLocation a, MapLocation b) {
+        return ((long)Math.pow((a.getX() - b.getX()), 2)) + ((long)Math.pow((a.getY() - b.getY()), 2));
     }
     
     // lazy way
@@ -96,5 +103,55 @@ public class Utils
     public static boolean canMoveWiggle(GameController gc, int unitId, Direction dir) {
         Direction[] neighboring = getAdjacentDirs(dir);
         return gc.canMove(unitId, dir) || gc.canMove(unitId, neighboring[0]) || gc.canMove(unitId, neighboring[1]);
+    }
+
+    public static ArrayList<Direction> directionList = new ArrayList<Direction>(Arrays.asList(Direction.North, Direction.Northeast, Direction.East, Direction.Southeast, Direction.South, Direction.Southwest, Direction.West, Direction.Northwest));
+
+    public static int[] smallRotation = new int[] {0, -1, 1};
+    public static int[] bigRotation = new int[] {0, -1, 1, -2, 2};
+
+    public static int tryMoveRotate(GameController gc, Unit unit, Direction direction) {
+        if (!gc.isMoveReady(unit.id())) {
+            return -1;   
+        }
+        int index = directionList.indexOf(direction);
+        for (int i = 0; i < bigRotation.length; i++) {
+            Direction tryDirection = directionList.get((8 + index + bigRotation[i]) % 8);
+            if (gc.canMove(unit.id(), tryDirection)) {
+                gc.moveRobot(unit.id(), tryDirection);
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static boolean canOccupy(GameController gc, MapLocation location, PlanetController parent, HashSet<MapLocation> visited) {
+        if (visited.contains(location)) {
+            return true;
+        }
+        PlanetMap map = ((EarthController)parent).earthMap;
+        boolean status = map.onMap(location) && map.isPassableTerrainAt(location) == 1 && !gc.hasUnitAtLocation(location);
+        if (status) {
+            visited.add(location);
+        }
+        return status;
+    }
+
+    public static boolean canOccupy(GameController gc, MapLocation location, PlanetController parent, UnitType type, HashSet<MapLocation> visited) {
+        if (visited.contains(location)) {
+            return true;
+        }
+        PlanetMap map = ((EarthController)parent).earthMap;
+        boolean status = !map.onMap(location) || map.isPassableTerrainAt(location) == 0 || !gc.hasUnitAtLocation(location) || gc.senseUnitAtLocation(location).unitType() != type;
+        if (status) {
+            visited.add(location);
+        }
+        return status;
+    }
+    
+    public static boolean canOccupyMars(GameController gc, MapLocation location) {
+        PlanetMap map = gc.startingMap(Planet.Mars);
+        boolean status = map.onMap(location) && map.isPassableTerrainAt(location) == 0 && !gc.hasUnitAtLocation(location);
+        return status;
     }
 }
--- seeding-bot/WorkerHandler.java
+++ seeding-bot/WorkerHandler.java
@@ -1,13 +1,352 @@
 import bc.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.ListIterator;
 import java.util.Random;
 
+// TODO:
+// smarter factory placement
+// smarter harvesting mechanics
+// Mars
+// smarter replication code
+
 public class WorkerHandler extends UnitHandler {
+    
+    private MapLocation previousLocation = null;
 
     public WorkerHandler(PlanetController parent, GameController gc, int id, Random rng) {
         super(parent, gc, id, rng);
     }
     
     public void takeTurn() {
-        // TODO
+        takeTurn(gc.unit(this.id));
+    }        
+    
+    @Override
+    public void takeTurn(Unit unit) {        
+
+        Location location = unit.location();
+
+        if (!location.isOnMap()) {            
+            return;
+        }
+
+        MapLocation mapLocation = location.mapLocation();
+
+        if (location.isOnPlanet(Planet.Mars)) {
+            return;
+        }
+
+        EarthController earthParent = (EarthController)parent;
+        boolean stationary = false;        
+
+        PlanetMap map = earthParent.earthMap;
+        HashMap<String, Long> moneyCount = earthParent.moneyCount;        
+        
+
+        VecUnit nearbyFriendly = gc.senseNearbyUnitsByTeam(mapLocation, unit.visionRange(), gc.team());
+        VecUnit nearbyEnemies = gc.senseNearbyUnitsByTeam(mapLocation, unit.visionRange(), Utils.getOtherTeam(gc.team()));
+
+        int nearbyFactoryCount = 0;
+        int nearbyBuiltFactoryCount = 0;
+        int nearbyWorkerCount = 0;
+        for (int i = 0; i < nearbyFriendly.size(); i++) {
+            if (nearbyFriendly.get(i).unitType() == UnitType.Worker) {
+                nearbyWorkerCount++;
+            } else if (nearbyFriendly.get(i).unitType() == UnitType.Factory) {
+                nearbyFactoryCount++;
+                if (nearbyFriendly.get(i).structureIsBuilt() == 1) {
+                    nearbyBuiltFactoryCount++;
+                }
+            }
+        }
+
+        if (nearbyFactoryCount > 0 && nearbyWorkerCount < 3) { 
+            // System.out.println("Early game replication");
+            for (Direction d : Utils.directionList) {
+                if (gc.canReplicate(unit.id(), d)) {
+                    gc.replicate(unit.id(), d);     
+                    earthParent.incrementRobotCount(UnitType.Worker);
+                    break;
+                }
+            }
+        }
+
+        if (gc.round() > 600 || gc.getTimeLeftMs() < 1000 ) {            
+            VecUnit nearbyRockets = gc.senseNearbyUnitsByType(mapLocation, unit.visionRange(), UnitType.Rocket);
+            Unit nearestRocket = null;
+            int nearestDistanceRocket = Integer.MAX_VALUE;
+            for (int j = 0; j < nearbyRockets.size(); j++) {
+                Unit nearbyUnit = nearbyRockets.get(j);
+                PathField path = earthParent.pm.getPathField(nearbyUnit.location().mapLocation());
+                if (nearbyUnit.structureIsBuilt() == 1 || !path.isPointSet(mapLocation)) {
+                    continue;
+                }
+                if (gc.canBuild(unit.id(), nearbyUnit.id())) {                
+                    // System.out.println("Building factory!");                
+                    gc.build(unit.id(), nearbyUnit.id());   
+                    stationary = true;             
+                    break;
+                }
+                if (nearbyUnit.team() == gc.team()) {
+                    int distance = (int)mapLocation.distanceSquaredTo(nearbyUnit.location().mapLocation());
+                    if (distance < nearestDistanceRocket || (distance == nearestDistanceRocket && rng.nextBoolean())) {
+                        nearestRocket = nearbyUnit;
+                        nearestDistanceRocket = distance;
+                    }
+                }
+            }            
+            if (!stationary && nearestRocket != null) {
+                MapLocation target = nearestRocket.location().mapLocation();
+                // PathField path = earthParent.pm.getPathField(target);
+                if (Utils.tryMoveRotate(gc, unit, mapLocation.directionTo(target)) != -1) {
+                    stationary = true;                    
+                }
+            }      
+            if (stationary) {
+                      return;
+                  }      
+            for (Direction d : Utils.directionList) {  
+                if (gc.canBlueprint(unit.id(), UnitType.Rocket, d)) {
+                    gc.blueprint(unit.id(), UnitType.Rocket, d);
+                    break;
+                }                
+            }        
+            return;            
+        }        
+
+        VecUnit nearbyFactories = gc.senseNearbyUnitsByType(mapLocation, unit.visionRange(), UnitType.Factory);
+        Unit nearestFactory = null;
+        int nearestDistanceFactory = Integer.MAX_VALUE;
+        for (int j = 0; j < nearbyFactories.size(); j++) {
+            Unit nearbyUnit = nearbyFactories.get(j);
+            PathField path = earthParent.pm.getPathField(nearbyUnit.location().mapLocation());
+            if (nearbyUnit.structureIsBuilt() == 1 || !path.isPointSet(mapLocation)) {
+                continue;
+            }
+            if (gc.canBuild(unit.id(), nearbyUnit.id())) {                
+                // System.out.println("Building factory!");                
+                gc.build(unit.id(), nearbyUnit.id());   
+                stationary = true;             
+                break;
+            }
+            if (nearbyUnit.team() == gc.team()) {
+                int distance = (int)mapLocation.distanceSquaredTo(nearbyUnit.location().mapLocation());
+                if (distance < nearestDistanceFactory || (distance == nearestDistanceFactory && rng.nextBoolean())) {
+                    nearestFactory = nearbyUnit;
+                    nearestDistanceFactory = distance;
+                }
+            }
+        }
+        if (!stationary && nearestFactory != null) {
+            MapLocation target = nearestFactory.location().mapLocation();
+            // PathField path = earthParent.pm.getPathField(target);
+            if (Utils.tryMoveRotate(gc, unit, mapLocation.directionTo(target)) != -1) {
+                stationary = true;
+                previousLocation = mapLocation; 
+            }
+        }
+
+        if (!stationary && nearbyEnemies.size() >= (int)((nearbyFriendly.size() - nearbyFactoryCount - nearbyWorkerCount) * 1.5)) {
+            Unit nearestEnemy = null;
+            int nearestDistanceEnemy = Integer.MAX_VALUE;
+            for (int j = 0; j < nearbyEnemies.size(); j++) {
+                Unit nearbyUnit = nearbyEnemies.get(j);                                
+                int distance = (int)mapLocation.distanceSquaredTo(nearbyUnit.location().mapLocation());
+                if (distance < nearestDistanceEnemy || (distance == nearestDistanceEnemy && rng.nextBoolean())) {
+                    nearestEnemy = nearbyUnit;
+                    nearestDistanceEnemy = distance;
+                }                
+            }            
+            if (!stationary && nearestEnemy != null && Utils.tryMoveRotate(gc, unit, mapLocation.directionTo(mapLocation.subtract(mapLocation.directionTo(nearestEnemy.location().mapLocation())))) != -1) {
+                stationary = true;
+            }            
+        }
+
+        if (!stationary && gc.karbonite() >= 100 && (earthParent.getRobotCount(UnitType.Factory) == 0 || (earthParent.getRobotCount(UnitType.Factory) >= 3 && nearbyFactoryCount == 0) || nearbyFactoryCount > 0)) {
+            Direction buildDirection = findBuildDirection(unit);            
+            if (buildDirection != null && gc.canBlueprint(unit.id(), UnitType.Factory, buildDirection)) {
+                // System.out.println("Blueprinting factory!");
+                gc.blueprint(unit.id(), UnitType.Factory, buildDirection);
+                earthParent.incrementRobotCount(UnitType.Factory);
+                stationary = true;                
+            }
+        }                
+
+        if (!stationary) {
+            LinkedList<MapLocation> moneyLocations = earthParent.moneyLocations;
+            ListIterator<MapLocation> iterator = moneyLocations.listIterator();
+            MapLocation nearestMoney = null;
+            long nearestDistance = Integer.MAX_VALUE;            
+            MapLocation mostMoneyLocation = null;
+            long mostMoney = 0;
+            while (iterator.hasNext()) {
+                MapLocation tryLocation = iterator.next();
+                if (tryLocation.isWithinRange(unit.visionRange(), mapLocation)) {
+                    long money = gc.karboniteAt(tryLocation);
+                    if (money <= 0) {
+                        iterator.remove();
+                        continue;
+                    }
+                    if (mostMoneyLocation == null || money > mostMoney) {
+                        mostMoney = money;
+                        mostMoneyLocation = tryLocation;                        
+                    }
+                }
+                long distance = mapLocation.distanceSquaredTo(tryLocation);
+                if (nearestMoney == null || distance < nearestDistance) {
+                    nearestDistance = distance;
+                    nearestMoney = mostMoneyLocation;
+                }
+            }            
+            if (mostMoneyLocation != null && Utils.tryMoveRotate(gc, unit, mapLocation.directionTo(mostMoneyLocation)) != -1) {
+                stationary = true;
+            } else if (nearestMoney != null && Utils.tryMoveRotate(gc, unit, mapLocation.directionTo(nearestMoney)) != -1) {
+                stationary = true;
+            }
+            // try {
+                // MapLocation nearestMoney = null;
+                // int nearestDistance = Integer.MAX_VALUE;            
+                // for (String locationKey : moneyCount.keySet()) {                
+                //     MapLocation tryLocation = bc.bcMapLocationFromJson(locationKey);                
+                //     // long distance = location.distanceSquaredTo(tryLocation);
+                //     // if (distance < nearestDistance || (distance == nearestDistance && rng.nextBoolean())) {
+                //     //     nearestMoney = tryLocation;
+                //     //     nearestDistance = distance;
+                //     // }
+                //     PathField path = earthParent.pm.getPathField(tryLocation);
+                //     if (path.isPointSet(location)) {                    
+                //         int distance = path.getDistanceAtPoint(location);                    
+                //         if (distance < nearestDistance) {                        
+                //             nearestMoney = tryLocation;
+                //             nearestDistance = distance;                        
+                //         }
+                //     }                
+                // }                
+
+                // if (nearestMoney != null) {
+                //     Direction d = earthParent.pm.getPathField(nearestMoney).getDirectionAtPoint(location);
+                //     // System.out.println("test");
+                //     if (Utils.tryMoveRotate(gc, unit, d) != -1) {
+                //         stationary = true;                    
+                //     }
+                // }
+            // } catch (Exception e) {e.printStackTrace();}
+        }
+
+        Direction bestDirection = null;
+        String bestLocationKey = null;
+        long mostMoney = 0;
+        for (Direction d : Direction.values()) {                         
+            MapLocation tryLocation = mapLocation.add(d);
+            String locationKey = tryLocation.toJson();            
+            if (moneyCount.containsKey(locationKey)) {
+                long money = gc.karboniteAt(tryLocation);
+                if (money <= 0) {
+                    moneyCount.remove(locationKey);
+                    continue;
+                } else {
+                    moneyCount.put(locationKey, money);       
+                    if (gc.canHarvest(unit.id(), d) && (bestDirection == null || money > mostMoney)) {
+                        bestDirection = d;
+                        bestLocationKey = locationKey;
+                        mostMoney = money;
+                    }
+                }
+            }            
+        }
+        if (bestDirection != null) {
+            // System.out.println("Harvesting");
+            gc.harvest(unit.id(), bestDirection);            
+            if (mostMoney <= unit.workerHarvestAmount()) {
+                moneyCount.remove(bestLocationKey);                
+            } else {
+                moneyCount.put(bestLocationKey, mostMoney - unit.workerHarvestAmount());
+            }            
+        }
+
+        // VecUnit nbors = gc.senseNearbyUnitsByType(mapLocation, 2, UnitType.Factory);
+        // for(int i = 0; i < nbors.size(); i ++) {
+        //     if(nbors.get(i).team() != gc.team()) continue;
+        //     if(gc.canRepair(this.id, nbors.get(i))) {
+        //         gc.repair(this.id, nbors.get(i));
+        //         break;
+        //     }
+        // }
+
+        if (stationary) {
+            return;
+        }
+
+        Direction moveDirection = findMoveDirection(unit);
+        if (moveDirection != null && Utils.tryMoveRotate(gc, unit, moveDirection) != -1) {
+            previousLocation = mapLocation;
+        }                        
+    }
+
+    private Direction findMoveDirection(Unit unit) {        
+        HashSet<MapLocation> tried = new HashSet<MapLocation>();
+        Direction bestDirection = null;
+        int mostEmpty = 0;
+        for (Direction d : Utils.directionList) {                        
+            MapLocation tryLocation = mapLocation.add(d);                
+            if (previousLocation != null && tryLocation.equals(previousLocation)) {
+                continue;
+            }
+            if (Utils.canOccupy(gc, tryLocation, earthParent, tried)) {
+                int empty = 0;
+                for (Direction dd : Utils.directionList) {
+                    MapLocation tryTryLocation = tryLocation.add(dd);
+                    if (Utils.canOccupy(gc, tryLocation, earthParent, tried)) {
+                        empty++;
+                    }
+                }                
+                if (bestDirection == null || (empty > mostEmpty || (empty == mostEmpty && rng.nextBoolean()))) {
+                    bestDirection = d;
+                    mostEmpty = empty;
+                }
+            }            
+        }
+        return bestDirection;
+    }
+
+    private Direction findBuildDirection(Unit unit) {
+        HashSet<MapLocation> tried = new HashSet<MapLocation>();
+        HashSet<MapLocation> triedTried = new HashSet<MapLocation>();
+        Direction bestDirection = null;
+        int mostEmpty = 0;                        
+        for (Direction d : Utils.directionList) {
+            MapLocation tryLocation = mapLocation.add(d);                
+            if (Utils.canOccupy(gc, tryLocation, earthParent, tried)) {
+                if (!testBuildLocation(tryLocation, d, triedTried)) {
+                    continue;
+                }
+                int empty = 0;                
+                for (Direction dd : Utils.directionList) {
+                    MapLocation tryTryLocation = tryLocation.add(dd);
+                    if (Utils.canOccupy(gc, tryLocation, earthParent, tried)) {
+                        empty++;
+                    } 
+                }                
+                if (bestDirection == null || (empty > mostEmpty || (empty == mostEmpty && rng.nextBoolean()))) {
+                    bestDirection = d;
+                    mostEmpty = empty;
+                }
+            }            
+        }        
+        return bestDirection;
+    }
+
+    private boolean testBuildLocation(MapLocation location, Direction direction, HashSet<MapLocation> triedTried) {
+        boolean status = true;        
+        for (int i = 0; i < Utils.directionList.size(); i++) {
+            Direction tryDirection = Utils.directionList.get(i);
+            MapLocation tryLocation = location.add(tryDirection);                        
+            status = status && Utils.canOccupy(gc, tryLocation, earthParent, UnitType.Factory, triedTried);   
+        }        
+        return status;
     }
 }
