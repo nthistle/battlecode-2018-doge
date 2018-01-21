c34c473e989901b81f93fd5eca2eaeef047efddd
Neil Thistlethwaite <neilt1111@yahoo.com>
2018-01-21 16:27:34 -0500

    Update seeding bot utils

--- seeding-bot/Utils.java
+++ seeding-bot/Utils.java
@@ -80,6 +80,46 @@ public class Utils
         return null;
     }
     
+    public static boolean tryMoveWiggleRecur(GameController gc, int unitID, Direction dir, Set<Integer> visited) {
+        if(!gc.isMoveReady(unitID) || dir == Direction.Center) return false;
+        if(visited != null && visited.contains(unitID)) return false;
+        if(gc.canMove(unitID, dir)) {
+            gc.moveRobot(unitID, dir);
+            return true;
+        }
+        else {
+            //attempt straight-on recursion
+            MapLocation myLocation = gc.unit(unitID).location().mapLocation();
+            if(visited == null) visited = new HashSet<Integer>();
+            visited.add(unitID);
+            if(gc.hasUnitAtLocation(myLocation.add(dir))) {
+                // System.out.println("Trying to recur move in " + dir);
+                Unit bih = gc.senseUnitAtLocation(myLocation.add(dir));
+                // System.out.println("before: " + bih);
+                if(Utils.tryMoveWiggleRecur(gc, bih.id(), dir, visited)) {
+                    // System.out.println("after: " + bih);
+                    gc.moveRobot(unitID, dir);
+                    // System.out.println("Success!");
+                    return true;
+                }
+            }
+            
+            //attempt moving to adjacencies
+            Direction[] adj = Utils.getAdjacentDirs(dir);
+            for(Direction c : adj) {
+                // System.out.println("Re-trying to recur move in " + c);
+                if(gc.hasUnitAtLocation(myLocation.add(c))) {
+                    if(Utils.tryMoveWiggleRecur(gc, gc.senseUnitAtLocation(myLocation.add(c)).id(), c, visited)) {
+                        // System.out.println("Success!");
+                        gc.moveRobot(unitID, c);
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+    
     // tries to move in dir, if fail, trys two adjacent dirs
     // returns 0 if move failed, 1 if original dir succeeded,
     // 2 if counter-clockwise neighbor succeeded,
@@ -111,5 +151,78 @@ public class Utils
     public static boolean canMoveWiggle(GameController gc, int unitId, Direction dir) {
         Direction[] neighboring = getAdjacentDirs(dir);
         return gc.canMove(unitId, dir) || gc.canMove(unitId, neighboring[0]) || gc.canMove(unitId, neighboring[1]);
+    }
+
+    public static boolean compareMapLocation(MapLocation a, MapLocation b) {
+        return a.getPlanet() == b.getPlanet() && a.getX() == b.getX() && a.getY() == b.getY();
+    }
+
+    public static boolean canMoveWiggle(GameController gc, int unitId, Direction dir) {
+        Direction[] neighboring = getAdjacentDirs(dir);
+        return gc.canMove(unitId, dir) || gc.canMove(unitId, neighboring[0]) || gc.canMove(unitId, neighboring[1]);
+    }
+
+    public static ArrayList<Direction> directionList = new ArrayList<Direction>(Arrays.asList(Direction.North, Direction.Northeast, Direction.East, Direction.Southeast, Direction.South, Direction.Southwest, Direction.West, Direction.Northwest));
+
+    public static int[] smallRotation = new int[] {0, -1, 1};
+    public static int[] mediumRotation = new int[] {0, -1, 1, -2, 2};
+    public static int[] bigRotation = new int[] {0, -1, 1, -2, 2, -3, 3, 4};
+
+    public static boolean tryMoveRotate(GameController gc, int id, Direction direction) {
+        if (!gc.isMoveReady(id)) {
+            return false;   
+        }
+        int index = directionList.indexOf(direction);
+        for (int i = 0; i < mediumRotation.length; i++) {
+            Direction tryDirection = directionList.get((8 + index + mediumRotation[i]) % 8);
+            if (gc.canMove(id, tryDirection)) {
+                gc.moveRobot(id, tryDirection);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean tryReplicateRotate(GameController gc, int id, Direction direction) {
+        int index = directionList.indexOf(direction);
+        for (int i = 0; i < bigRotation.length; i++) {
+            Direction tryDirection = directionList.get((8 + index + bigRotation[i]) % 8);
+            if (gc.canReplicate(id, tryDirection)) {
+                gc.replicate(id, tryDirection);
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public static boolean canOccupy(GameController gc, MapLocation location, PlanetController parent, HashSet<MapLocation> visited) {
+        if (visited.contains(location)) {
+            return true;
+        }
+        PlanetMap map = ((EarthController)parent).map;                
+        boolean status = map.onMap(location) && map.isPassableTerrainAt(location) == 1 && !gc.hasUnitAtLocation(location);
+        if (status) {
+            visited.add(location);
+        }
+        return status;
+    }
+
+    public static boolean canOccupy(GameController gc, MapLocation location, PlanetController parent, UnitType type, HashSet<MapLocation> visited) {
+        if (visited.contains(location)) {
+            return true;
+        }
+        PlanetMap map = ((EarthController)parent).map;
+        boolean status = !map.onMap(location) || map.isPassableTerrainAt(location) == 0 || !gc.hasUnitAtLocation(location) || gc.senseUnitAtLocation(location).unitType() != type;
+        if (status) {
+            visited.add(location);
+        }
+        return status;
+    }
+    
+    public static boolean canOccupyMars(GameController gc, MapLocation location) {
+        PlanetMap map = gc.startingMap(Planet.Mars);
+        boolean status = map.onMap(location) && map.isPassableTerrainAt(location) == 0 && !gc.hasUnitAtLocation(location);
+        return status;
     }
 }
