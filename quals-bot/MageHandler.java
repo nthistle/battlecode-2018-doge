35363c6a114aca5d5dc25ff18cd77389a4253dc4
Neil Thistlethwaite <neilt1111@yahoo.com>
2018-01-28 17:48:59 -0500

    Mage WIP

--- quals-bot/MageHandler.java
+++ quals-bot/MageHandler.java
@@ -5,6 +5,9 @@ import java.util.Random;
 
 public class MageHandler extends UnitHandler {
 
+    private static final double LATERAL_MOVE_CHANCE = 0.4;
+    private boolean lastWasLat = false;
+
     public MageHandler(PlanetController parent, GameController gc, int id, Random rng) {
         super(parent, gc, id, rng);
     }
@@ -20,11 +23,56 @@ public class MageHandler extends UnitHandler {
     }
 
     public void handleMovement(Unit unit) {
+
+        MapLocation myLoc = unit.location().mapLocation();
+        PlanetMap pmap = gc.startingMap(gc.planet());
+
+        MapLocation target = parent.tm.getTarget(myLoc);
+        if(target == null) return;
+
+        PathField pf = parent.pm.getCachedPathField(target);
+        if(pf == null) {
+            if(parent.tm.getTarget(0) != null)
+                pf = parent.pm.getCachedPathField(parent.tm.getTarget(0));
+        }
+        if(pf == null) return;
+
         List<Direction> closerDirs = new ArrayList<Direction>();
         List<Direction> sameDirs = new ArrayList<Direction>();
+
+        int myDist = pf.getDistanceAtPoint(myLoc);
+
         for(Direction dir : Utils.directions()) {
-            // pass
+            MapLocation newLoc = myLoc.add(dir);
+            if(!pmap.onMap(newLoc)) continue;
+            if(pf.getDistanceAtPoint(newLoc) < myDist) {
+                closerDirs.add(dir);
+            } else if(pf.getDistanceAtPoint(newLoc) == myDist) {
+                sameDirs.add(dir);
+            }
         }
+
+        boolean movingLat = !lastWasLat && (this.rng.nextDouble() < LATERAL_MOVE_CHANCE);
+        this.lastWasLat = false;
+
+        if(movingLat) {
+            for(Direction dir : sameDirs) {
+                if(gc.canMove(this.id, dir)) {
+                    gc.moveRobot(this.id, dir);
+                    this.lastWasLat = true;
+                    return;
+                }
+            }
+        } else {
+            for(Direction dir : closerDirs) {
+                if(gc.canMove(this.id, dir)) {
+                    gc.moveRobot(this.id, dir);
+                    return;
+                }
+            }
+        }
+
+
     }
 
     public Direction getRandomDirection(MapLocation mapLocation, MapLocation targetLocation, PathMaster pm) {
